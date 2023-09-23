9697ce0a83c2e4cac779f6f980efd5edbcd59b05
CarlosGranados90 <carlosd.granadosm@gmail.com>
2023-09-23 11:16:22 -0500

    Se agrega el proyecto

---
+++ Farmacia/src/main/main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+import views.LoginView;
+
+public class main {
+    public static void main(String[] args) {
+        //Instancia del login
+        LoginView login = new LoginView();
+        login.setVisible(true); // hacer visible el objeto
+        
+    }
+}
